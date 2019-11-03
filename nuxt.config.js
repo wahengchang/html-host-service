fd11dbf4308397208dbab106e26741d56b97b84b
peter chang <wahengchang@gmail.com>
2019-11-03 16:04:56 +0800

    added pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ module.exports = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
   ],
   /*
    ** Axios module configuration
